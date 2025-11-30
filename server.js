dddcce64db04a2988866c49450b88662a9b31733
Francrack22 <fortega46910@ucq.edu.mx>
2025-11-29 20:06:11 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const { Pool } = require('pg');
-const bcrypt = require = require('bcrypt');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config();
